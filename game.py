da056b9d97ba40f64bfa3c9f10ac4c76a6f28912
Baptiste7512 <80527980+Baptiste7512@users.noreply.github.com>
2022-04-13 15:33:18 +0200

    resolution beug affichage carte + création de perso ne pouvant pas bouger pour le moment

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import pygame
 import pytmx
 import pyscroll
 
+from Firstgame.player import Player
+
 
 class Game:
     def __init__(self):
@@ -11,14 +13,20 @@ class Game:
         pygame.display.set_caption("First Game")
 
         # Charge la carte
-        tmx_data = pytmx.util_pygame.load_pygame("Map.tmx")
+        tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
 
         # Recupère la taille
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
 
+        # Genere le player
+        # Recupere l'objet instancier du joueur dans tiled qui permet de le faire pop
+        player_position = tmx_data.get_object_by_name("playeurstart")
+        self.player = Player(player_position.x, player_position.y)
+
         # layer
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.group.add(self.player)
 
     def run(self):
 
@@ -26,6 +34,8 @@ class Game:
         running = True
         while running:
 
+            self.group.update()
+            self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
             pygame.display.flip()
 
